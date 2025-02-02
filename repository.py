b2c098e2d4c79b3d969ebfc68f3522280860be77
fabgal-p1 <>
2025-02-02 22:37:03 +0100

    - Add yfinance import in repository

--- repository.py
+++ repository.py
@@ -1,16 +1,16 @@
 import numpy as np
-import pandas as pd
+import yfinance as yf
 
 portfolio = {"GE": 0.5, "JPM": 0.2, "MSFT": 0.2, "PG": 0.1}
+field_to_keep = "Close"
+begin_date = "2015-01-02"
+end_date = "2018-03-27"
 
 
 def get_data():
-    return pd.read_csv(
-        r"input\small_portfolio.csv",
-        delimiter=",",
-        index_col="date",
-        parse_dates=["date"],
-    )
+    tickers = list(portfolio.keys())
+    data = yf.download(tickers, start=begin_date, end=end_date)
+    return data[field_to_keep]
 
 
 def get_weights() -> np.array:
